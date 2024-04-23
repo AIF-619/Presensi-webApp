ef438894a2c7df1d213667438dd6ed1a2a81b4b7
RayvanoPutra <rayvano159@gmail.com>
2024-04-23 16:58:04 +0700

    tester

---
+++ storage/framework/views/2985e2bfbbd213301b72947be2a70b2dea237d3a.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    
+    <link rel="stylesheet" href="css/home-kelompok-3.css">
+    
+
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+  <div class="kontainer">
+
+    <div class="sidebar">
+        <div class="header">
+            <div class="list-item">
+                <a href="#">
+                    <img src="" alt="" class="icon">
+                    <span class="deskirpsi-header">Hadir</span>
+                </a>
+            </div>
+
+            <div class="ilustrasi">
+                <img src="img/time.png" alt="">
+            </div>
+        </div>
+        <div class="main">
+            <div class="list-item">
+                <a href="">
+                    <img src="" alt="" class="icon">
+                    <span class="deskripsi">beranda</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-konten">
+        <h1>jancok</h1>
+    </div>
+
+    
+  </div>
+    <script src="js/kelompok-3.js"> </script>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\kelompok-3-aja\resources\views/home.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/afa532518b80944b03ad609e07b281587d418720.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hadir</title>
+
+    
+    <link rel="stylesheet" href="css/kelompok-3.css">
+    
+
+    
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    
+
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+   <div class="logo-lab" style="position: absolute; top: 30px; left: 30px;">
+    <img src="img/Logo-lab.png" alt="" style="width: 90px;">
+   </div>
+
+    <div class="wrapper">
+        <form action="">
+            <h1>Login</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Username" required>
+            <i class="bx bxs-user"></i>
+            </div>
+            <div class="input-box">
+                <input type="Password" placeholder="Password" required>
+                <i class="bx bxs-lock-alt"></i>
+            </div>
+            <button type="submit" class="btn">Login</button>
+        </form>
+    </div>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\kelompok-3-aja\resources\views/login.blade.php ENDPATH**/ ?>
